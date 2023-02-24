4ac6daa36ec9c2aee06b5c91b80b8dd25ae73322
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-02-24 19:33:08 +0100

    Menu  responsive

---
+++ menuResponsive/index.js
@@ -0,0 +1,17 @@
+const navUl = document.querySelector(".nav__ul");
+const navResponsive = document.querySelector(".nav__responsive-ul");
+
+//Funcion cambio tamano
+const navChange = () => {
+    if(screen.width < 500){
+        navUl.style.display = "none";
+        navResponsive.style.display = "block";
+    }else{
+        navUl.style.display = "inline-block";
+        navResponsive.style.display = "none";
+    }
+}
+//Metodo para controlar el cambio de tamano de la pantalla
+window.addEventListener("resize", navChange);
+
+
