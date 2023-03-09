f38e32db0b897e87ed24e9ea08b3ab8ec89f3e3d
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-03-09 13:18:26 +0100

    Cambios en el formulario

--- practicandoCSS/formulario.js
+++ practicandoCSS/formulario.js
@@ -11,19 +11,38 @@ const regUserEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 const errors = [];
 
 const showErrors = (errors) => {
-
+ 
   errors.forEach((error) => {
     error.type.textContent = error.msg;
   })
 }
-const showSuccesMessage = (errors) => {
+const cleanErrorsName = (errors) => {
        errors.forEach(error => {
-        error.type.textContent = "";
+        if(error.type === alertName){
+            error.msg = "";
+        }
        })
 }
-form.addEventListener("submit", (e) => {
+const cleanErrorsEmail = (errors) => {
+    errors.forEach(error => {
+        if(error.type === alertEmail){
+            error.msg = "";
+        }
+    })
+}
+const checkForm = (errors) => {
+    let check;
+    errors.forEach( error => {
+        if(error.msg === "")
+        check = true;
+        else
+        check = false;
+    })
 
-    e.preventDefault();
+    return check;   
+
+}
+form.addEventListener("submit", (e) => {
 
     if(!regUserName.test(userName.value) || !userName.value.trim()){
         console.log("Formato de nombre incorrecto");
@@ -32,6 +51,8 @@ form.addEventListener("submit", (e) => {
             type: alertName,
             msg: "Formato no valido en el campo nombre, solo letras"
         })
+    }else{
+        cleanErrorsName(errors)
     }
     
     if(!regUserEmail.test(userEmail.value) || !userEmail.value.trim()){
@@ -40,14 +61,17 @@ form.addEventListener("submit", (e) => {
             type: alertEmail, 
             msg: "Formato de correo electronico no valido"
         })
+    }else{
+        cleanErrorsEmail(errors);
     }
      
     if(errors.length !== 0){
         showErrors(errors);
+        e.preventDefault();
         //With the stop 
-        return;
+        return
     }
-    showSuccesMessage(errors);
+    
     console.log("Formato valido");
 })
 
