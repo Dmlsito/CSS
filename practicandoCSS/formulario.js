377a5735547f35be3fb7cd88a69dcbf08789e32b
Dmlsito <101351451+Dmlsito@users.noreply.github.com>
2023-03-09 19:12:42 +0100

    Cambios en el portfolio, borrado de algunos metodos y edicion de laas cards de los divs

--- practicandoCSS/formulario.js
+++ practicandoCSS/formulario.js
@@ -8,43 +8,19 @@ const alertSuccess = document.querySelector(".alert-success");
 var regUserName =  /^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/;
 const regUserEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
-const errors = [];
-
 const showErrors = (errors) => {
  
   errors.forEach((error) => {
     error.type.textContent = error.msg;
   })
 }
-const cleanErrorsName = (errors) => {
-       errors.forEach(error => {
-        if(error.type === alertName){
-            error.msg = "";
-        }
-       })
-}
-const cleanErrorsEmail = (errors) => {
-    errors.forEach(error => {
-        if(error.type === alertEmail){
-            error.msg = "";
-        }
-    })
-}
-const checkForm = (errors) => {
-    let check;
-    errors.forEach( error => {
-        if(error.msg === "")
-        check = true;
-        else
-        check = false;
-    })
 
-    return check;   
-
-}
 form.addEventListener("submit", (e) => {
 
+    const errors = [];
+
     if(!regUserName.test(userName.value) || !userName.value.trim()){
+        alertName.style.display = "block"
         console.log("Formato de nombre incorrecto");
         //Add the error
         errors.push({
@@ -52,27 +28,27 @@ form.addEventListener("submit", (e) => {
             msg: "Formato no valido en el campo nombre, solo letras"
         })
     }else{
-        cleanErrorsName(errors)
+        alertName.style.display = "none";
     }
     
     if(!regUserEmail.test(userEmail.value) || !userEmail.value.trim()){
+        alertEmail.style.display = "block"
         console.log("Formato de email incorrecto");
         errors.push({
             type: alertEmail, 
             msg: "Formato de correo electronico no valido"
         })
     }else{
-        cleanErrorsEmail(errors);
+       alertEmail.style.display = "none";
     }
      
     if(errors.length !== 0){
         showErrors(errors);
         e.preventDefault();
-        //With the stop 
+        console.log("Formato no valido")
+        //With the stop we stop the form
         return
     }
-    
-    console.log("Formato valido");
 })
 
 
--- practicandoCSS/index.js
+++ practicandoCSS/index.js
@@ -4,14 +4,15 @@ const title1 = document.getElementById("title-img1");
 const title2 = document.getElementById("title-img2");
 
 img1.addEventListener('mouseover', () => {
-  title1.style.marginBottom = "40px";
+  title1.style.opacity = 0;
+  
 });
 img1.addEventListener("mouseout", () => {
-    title1.style.marginBottom = "0";
+    title1.style.opacity = 1;
 })
 img2.addEventListener("mouseover", () => {
-    title2.style.marginBottom = "40px";
+    title2.style.opacity = 0;
 })
 img2.addEventListener("mouseout", () => {
-    title2.style.marginBottom = "0";
+    title2.style.opacity = 1;
 })
